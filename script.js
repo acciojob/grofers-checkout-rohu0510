0774923a19623d7c33a09f29a73f2f23204f2603
Rohit Prajapati <65508466+rohu0510@users.noreply.github.com>
2023-02-22 20:22:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-	return "500";
+	const prices = document.querySelectorAll(".price");
+  
+  // Calculate the total price
+  let totalPrice = 0;
+  prices.forEach(price => {
+    totalPrice += parseFloat(price.textContent);
+  });
+  
+  // Add a new row to the table with the total price
+  const table = document.querySelector("table");
+  const newRow = table.insertRow(-1);
+  const newCell = newRow.insertCell(0);
+  newCell.textContent = `Total Price: ${totalPrice}`;
   
 };
 
